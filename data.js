9cf9e94e3fa823b4e579c1341be6e80f00168b29
Morgan Harmon <morganharmon@gmail.com>
2022-05-12 15:16:37 -0700

    set up readme, initial pages, and supabase data

---
+++ data.js
@@ -0,0 +1,23 @@
+export const juices = [
+    {
+        id: 1,
+        type: 'Carrot',
+        color: 'orange',
+        vitamins: 'beta-carotene',
+        image: './assets/carrot-juice.webp'
+    },
+    {
+        id: 2,
+        type: 'Apple',
+        color: 'brown',
+        vitamins: 'vitamin B6',
+        image: './assets/apple-juice.jpg'
+    },
+    {
+        id: 3,
+        type: 'Orange',
+        color: 'orange',
+        vitamins: 'vitamin C',
+        image: './assets/orange-juice.jpg'
+    }
+];
