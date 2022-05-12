497d02e9ec64852f363f9b8a1c93e4f4dcbeabd9
Morgan Harmon <morganharmon@gmail.com>
2022-05-12 16:38:05 -0700

    Add renderDetails function, pass test

--- render-utils.js
+++ render-utils.js
@@ -13,3 +13,18 @@ export function renderJuice(juice) {
     div.append(a, img);
     return div;
 }
+
+export function renderDetails(juice) {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+    const img = document.createElement('img');
+
+    h2.textContent = juice.type + ' juice';
+    img.src = juice.image;
+    img.alt = `${juice.type} juice`;
+    p.textContent = `${juice.type} juice is ${juice.color} and is full of ${juice.vitamins}.`;
+    
+    div.append(h2, img, p);
+    return div;
+}
