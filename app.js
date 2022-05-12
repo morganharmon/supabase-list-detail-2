92cc965177b51694b8a185e591cdbd05e025c7d2
Morgan Harmon <morganharmon@gmail.com>
2022-05-12 16:02:10 -0700

    render home page items, verified visually

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // import functions and grab DOM elements
 import { fetchAllData } from './fetch-utils.js';
-
+import { renderJuice } from './render-utils.js';
 // let state
+const juiceSection = document.getElementById('juiceSection');
 
 async function loadData() {
-    fetchAllData();
+    const juices = await fetchAllData();
+    for (let juice of juices) {
+        const div = renderJuice(juice);
+        juiceSection.append(div);
+    }
 }
 
 loadData();
